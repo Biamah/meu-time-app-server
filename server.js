834adcea56309c4e033f09c555bd2a7a1ec86691
Biamah <beatriz.souza.costa@outlook.com>
2023-05-25 11:16:07 -0300

    feat: add header to api get and error handling

--- server.js
+++ server.js
@@ -9,12 +9,21 @@ app.use(cors());
 app.get('/api/endpoint', async (req, res) => {
     try {
         const apiKey = req.query.api_key;
-        const apiUrl = `https://api-football.com/endpoint?api_key=${apiKey}`;
+        const apiUrl = `https://v3.football.api-sports.io/status`;
+        const headers = {
+            'x-rapidapi-key': apiKey,
+            'x-rapidapi-host': 'v3.football.api-sports.io',
+        }
 
-        const response = await axios.get(apiUrl);
+        const response = await axios.get(apiUrl, {headers});
         const data = response.data;
 
-        res.json(data);
+        if(data.results === 0) {
+            res.status(406).send('Key está incorreta');
+        } else {
+            res.json(data);
+        }
+
     } catch(error) {
         console.error('Ocorreu um erro ao fazer a solicitação', error);
         res.status(500).json({error: 'Ocorreu um erro ao fazer sua solicitação'});
