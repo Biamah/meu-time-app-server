a3dbb600b58a1c6b2f973f35bd4d784613fb8908
Biamah <beatriz.souza.costa@outlook.com>
2023-05-30 10:26:50 -0300

    feat: adds team route, filtered by league id and season

--- server.js
+++ server.js
@@ -1,82 +1,141 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
 app.use(cors());
 
-app.get('/api/endpoint', async (req, res) => {
-    try {
-        const apiKey = req.query.api_key;
-        const apiUrl = `https://v3.football.api-sports.io/status`;
-        const headers = {
-            'x-rapidapi-key': apiKey,
-            'x-rapidapi-host': 'v3.football.api-sports.io',
-        }
+app.get("/api/endpoint", async (req, res) => {
+  try {
+    const apiKey = req.query.api_key;
+    const apiUrl = `https://v3.football.api-sports.io/status`;
+    const headers = {
+      "x-rapidapi-key": apiKey,
+      "x-rapidapi-host": "v3.football.api-sports.io",
+    };
 
-        const response = await axios.get(apiUrl, {headers});
-        const data = response.data;
+    const response = await axios.get(apiUrl, { headers });
+    const data = response.data;
 
-        if(data.results === 0) {
-            res.status(406).send('Key está incorreta');
-        } else {
-            res.json(data);
-        }
-
-    } catch(error) {
-        console.error('Ocorreu um erro ao fazer a solicitação', error);
-        res.status(500).json({error: 'Ocorreu um erro ao fazer sua solicitação'});
+    if (data.results === 0) {
+      res.status(406).send("Key está incorreta");
+    } else {
+      res.json(data);
     }
-})
+  } catch (error) {
+    console.error("Ocorreu um erro ao fazer a solicitação", error);
+    res.status(500).json({ error: "Ocorreu um erro ao fazer sua solicitação" });
+  }
+});
 
-app.get('/country', async (req, res) => {
+app.get("/country", async (req, res) => {
+  try {
+    const apiCountry = req.query.api_country;
+    const apiUrl = `https://v3.football.api-sports.io/countries`;
+    const headers = {
+      "x-rapidapi-key": "77612f4d1285bd0d44bb4ab3be3a45d6",
+      "x-rapidapi-host": "v3.football.api-sports.io",
+    };
+
+    const params = {
+      name: apiCountry,
+    };
+
+    const response = await axios.get(apiUrl, { headers, params });
+    const data = response.data;
+
+    if (data.response === 0) {
+      res.status(404).send("O país digitado não foi encontrado");
+    } else {
+      res.json(data);
+    }
+  } catch (error) {
+    console.error("Ocorreu um erro ao fazer sua solicitação", error);
+    res.status(500).json({ error: "Ocorreu um erro ao fazer sua solicitação" });
+  }
+});
+
+app.get("/season", async (req, res) => {
+  try {
+    // const apiSeason = req.query.api_season;
+    const apiUrl = `https://v3.football.api-sports.io/leagues/seasons`;
+    const headers = {
+      "x-rapidapi-key": "77612f4d1285bd0d44bb4ab3be3a45d6",
+      "x-rapidapi-host": "v3.football.api-sports.io",
+    };
+
+    const response = await axios.get(apiUrl, { headers });
+    const data = response.data;
+
+    if (data.response === 0) {
+      res.status(404).send("As temporadas não foram encontradas");
+    } else {
+      res.json(data);
+    }
+  } catch (error) {
+    console.error("Ocorreu um erro ao fazer sua solicitação", error);
+    res.status(500).json({ error: "Ocorreu um erro ao fazer sua solicitação" });
+  }
+});
+
+app.get("/leagues", async (req, res) => {
+  try {
+    const apiCountry = req.query.api_country;
+    const apiSeason = req.query.api_season;
+    const apiUrl = `https://v3.football.api-sports.io/leagues`;
+    const headers = {
+      "x-rapidapi-key": "77612f4d1285bd0d44bb4ab3be3a45d6",
+      "x-rapidapi-host": "v3.football.api-sports.io",
+    };
+
+    const params = {
+      country: apiCountry,
+      season: apiSeason,
+    };
+
+    const response = await axios.get(apiUrl, { headers, params });
+    const data = response.data;
+
+    if (data.response === 0) {
+      res
+        .status(404)
+        .send("Não foram encontradas ligas para o país especificado");
+    } else {
+      res.json(data);
+    }
+  } catch (error) {
+    console.error("Ocorreu um erro ao fazer sua solicitação", error);
+    res.status(500).json({ error: "Ocorreu um erro ao fazer sua solicitação" });
+  }
+});
+
+app.get("/teams", async (req, res) => {
     try {
-        const apiCountry = req.query.api_country;
-        const apiUrl = `https://v3.football.api-sports.io/countries`;
+        const apiLeague = req.query.api_league;
+        const apiSeason = req.query.api_season;
+        const apiUrl = `https://v3.football.api-sports.io/teams`;
         const headers = {
-            'x-rapidapi-key': '77612f4d1285bd0d44bb4ab3be3a45d6',
-            'x-rapidapi-host': 'v3.football.api-sports.io',
-        }
+            "x-rapidapi-key": "77612f4d1285bd0d44bb4ab3be3a45d6",
+            "x-rapidapi-host": "v3.football.api-sports.io",
+        };
 
         const params = {
-            'name': apiCountry,
+            league: apiLeague,
+            season: apiSeason,
         }
 
         const response = await axios.get(apiUrl, { headers, params });
         const data = response.data;
 
-        if (data.response === 0) {
-            res.status(404).send('O país digitado não foi encontrado');
+        if(data.response === 0) {
+            res.status(404).send('Não foram encontrados times para a liga e temporada especificadas');
         } else {
             res.json(data);
         }
     } catch(error) {
         console.error('Ocorreu um erro ao fazer sua solicitação', error);
-        res.status(500).json({ error: 'Ocorreu um erro ao fazer sua solicitação' });
-    }
-})
-
-app.get('/season', async (req, res) => {
-    try {
-        // const apiSeason = req.query.api_season;
-        const apiUrl = `https://v3.football.api-sports.io/leagues/seasons`;
-        const headers = {
-            'x-rapidapi-key': '77612f4d1285bd0d44bb4ab3be3a45d6',
-            'x-rapidapi-host': 'v3.football.api-sports.io',
-        }
-
-        const response = await axios.get(apiUrl, { headers });
-        const data = response.data;
-
-        if (data.response === 0) {
-            res.status(404).send('As temporadas não foram encontradas');
-        } else {
-            res.json(data);
-        }
-    } catch(error) {
-        console.error('Ocorreu um erro ao fazer sua solicitação', error);
-        res.status(500).json({ error: 'Ocorreu um erro ao fazer sua solicitação' });
+        res.status(500).json({ error: 'Ocorreu um erro ao fazer sua solicitação' })
     }
 })
 
